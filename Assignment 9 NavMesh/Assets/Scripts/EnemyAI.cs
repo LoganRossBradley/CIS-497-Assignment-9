f62600717fc82f60b7b97f86ce87fefb17ec1c34
LoganRossBradley <60478655+LoganRossBradley@users.noreply.github.com>
2020-12-13 23:53:40 -0600

    Finished

---
+++ Assignment 9 NavMesh/Assets/Scripts/EnemyAI.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+using UnityStandardAssets.Characters.ThirdPerson;
+
+public class EnemyAI : MonoBehaviour
+{
+    public Camera cam;
+    public NavMeshAgent agent;
+    public ThirdPersonCharacter character;
+    public GameObject player;
+    public float chaseDistance;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        cam = Camera.main;
+        agent = GetComponent<NavMeshAgent>();
+        character = GetComponent<ThirdPersonCharacter>();
+        agent.updateRotation = false;
+        player = GameObject.FindGameObjectWithTag("Player");
+        chaseDistance = 8.0f;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        float distanceAway = Vector3.Distance(transform.position, player.transform.position);
+        if (distanceAway > agent.stoppingDistance && distanceAway < chaseDistance)
+        {
+            agent.SetDestination(player.transform.position);
+            character.Move(agent.desiredVelocity, false, false);
+        }
+        else
+        {
+            agent.SetDestination(transform.position);
+            character.Move(Vector3.zero, false, false);
+        }
+    }
+}
